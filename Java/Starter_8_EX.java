dfb5b8f50a7d197db22c3f61853abda20faad279
Weiss_rN <darma20060311@gmail.com>
2025-11-24 12:14:20 +0000

    2114E

--- Java/Starter_8_EX.java
+++ Java/Starter_8_EX.java
@@ -1,5 +1,10 @@
 public class Starter_8_EX {
     public static void main(String[] args) {
-        String 
+        String systemOSName =  "Linux Kernel 6.18-rc7"; //Example
+        long hash64 = 20251124210001L; //Example
+        int hash32 = 20251124; //Example
+        System.out.println("System OS Name: " + systemOSName);
+        System.out.println("64-bit Hash: " + hash64);
+        System.out.println("32-bit Hash: " + hash32);
     }
 }
